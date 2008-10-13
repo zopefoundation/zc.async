4774f1ece361d131830413579d06399a559440fd
Zvezdan Petkovic <zpetkovic@acm.org>
2008-10-13 22:06:09 +0000

    Alpha for the next development cycle (1.5.2)

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def text(*args, **kwargs):
 
 setup(
     name='zc.async',
-    version='1.5.1',
+    version='1.5.2a1',
     namespace_packages=['zc'],
     packages=find_packages('src'),
     package_dir={'':'src'},
