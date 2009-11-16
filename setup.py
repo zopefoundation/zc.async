68b5ec4fc28dd0875e9e22daa11341db907f7c53
Gary Poster <gary@modernsongs.com>
2009-11-16 01:31:20 +0000

    prepare trunk for dev

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def text(*args, **kwargs):
 
 setup(
     name='zc.async',
-    version='1.5.3',
+    version='1.5.4dev',
     namespace_packages=['zc'],
     packages=find_packages('src'),
     package_dir={'':'src'},
